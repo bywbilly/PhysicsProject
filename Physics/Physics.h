3018e4b3e57364800c4c3316ffe39a2cb2182a15
niconiconi <niconiconi@sjtu.edu.cn>
2015-06-27 19:37:44 +0800

    并没有改变什么

--- Physics/Physics.h
+++ Physics/Physics.h
@@ -4,27 +4,30 @@
 #include <algorithm>
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 class Simulator
 {
 public:
+	const double g = 9.8;
 	b2World *world;
 	void init()
 	{
-
+		world = new b2World(b2Vec2(0,g), false);
 	}
 	Simulator()
 	{
-
+		init();
 	}
 	~Simulator()
 	{
-
+		delete world;
 	}
-	/*return true if the polygon is valid*/
-	bool addPolygon()
-	{
 
+	/*return true if the polygon is valid*/
+	bool addPolygon(vector<b2Vec2> points, bool canMove)
+	{
+		
 	}
 
 	/*The return value should be a image*/
