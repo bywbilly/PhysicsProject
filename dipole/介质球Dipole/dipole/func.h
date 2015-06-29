eaf28a335f39f35289fb7ede0b4718b697456004
niconiconi <niconiconi@sjtu.edu.cn>
2015-06-29 23:36:50 +0800

    del

--- dipole/介质球Dipole/dipole/func.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNC_H_INCLUDED
-#define FUNC_H_INCLUDED
-#include "sys.h"
-#include "main_display.h"
-
-void system_moveonce(sys &s);
-void system_energy(sys &s);//×ÜÄÜÁ¿
-
-#endif // FUNC_H_INCLUDED
