79cd68f99bfefce648f2e28a9865a2e5119da661
pkq2006 <pkq2006@gmail.com>
2015-06-30 16:24:53 +0800

    update
    algorithm of center of mass update

--- Physics/Physics.h
+++ Physics/Physics.h
@@ -61,21 +61,29 @@ public:
 	/*return true if the polygon is valid*/
 	void addPolygon(vector<b2Vec2> points, bool canMove)
 	{
-        fprintf(stderr, "ADD POLYGON\n");
-		for(int i = 0; i < points.size(); ++i)
+		fprintf(stderr, "ADD POLYGON\n");
+		for (int i = 0; i < points.size(); ++i)
 			points[i].x *= P2M, points[i].y *= P2M;
 		points = convexhull(points);
 		fprintf(stderr, "check return\n");
-		if(points.size() < 3)
+		if (points.size() < 3)
 			return;
 		fprintf(stderr, "convexhull SUCCESS %d\n", (int)points.size());
 
 		b2BodyDef bodydef;
 		b2Vec2 Gcenter;
 		Gcenter = b2Vec2(0, 0);
-		for(int i = 0; i < points.size(); ++i)
-			Gcenter = Gcenter + points[i];
-		Gcenter.x /= points.size(), Gcenter.y /= points.size();
+		float area = 0;
+		for (int i = 1; i < (int)points.size() - 1; ++i)
+		{
+			float now_area = det(points[i] - points[0], points[i + 1] - points[0]);
+			area += now_area;
+			b2Vec2 now_point = points[0] + points[i] + points[i + 1];
+			now_point.x *= now_area / 3;
+			now_point.y *= now_area / 3;
+			Gcenter = Gcenter + now_point;
+		}
+		Gcenter.x /= area, Gcenter.y /= area;
 		bodydef.position.Set(Gcenter.x, Gcenter.y);
 		if(canMove)
 			bodydef.type=b2_dynamicBody;
